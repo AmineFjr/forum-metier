09e69ab546bf6711f60f2d32e6920411aaaa6845
Amine Fajry <fajry599@gmail.com>
2023-01-30 10:03:49 +0100

    feature: add composer packages

---
+++ src/Entity/Atelier.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AtelierRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AtelierRepository::class)]
+class Atelier
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $duree = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDuree(): ?string
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(string $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+}
